063f4834be265f6134c5e2c5e9ef62bd57766aa4
Andy Katz <andy.katz@bazaarvoice.com>
2012-05-25 16:30:09 -0500

    Adding SDK source files

---
+++ src/com/requiem/bazaarvoice/BazaarException.java
@@ -0,0 +1,16 @@
+package com.requiem.bazaarvoice;
+
+/**
+ * User: gary
+ * Date: 4/17/12
+ * Time: 10:46 PM
+ */
+public class BazaarException extends Exception {
+    public BazaarException(String detailMessage) {
+        super(detailMessage);
+    }
+
+    public BazaarException(String detailMessage, Throwable throwable) {
+        super(detailMessage, throwable);
+    }
+}
