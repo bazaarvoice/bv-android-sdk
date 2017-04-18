c2b6c4172f47fb7c146dfa130c7d17eab7736391
Casey Kulm <casey.kulm@bazaarvoice.com>
2017-04-18 12:58:10 -0500

    MOB-590: Conversations parse date as UTC

--- bvconversations/src/test/java/com/bazaarvoice/bvandroidsdk/DateUtilTest.java
+++ bvconversations/src/test/java/com/bazaarvoice/bvandroidsdk/DateUtilTest.java
@@ -2,25 +2,35 @@ package com.bazaarvoice.bvandroidsdk;
 
 import org.junit.Test;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class DateUtilTest {
 
     @Test
-    public void dateFromString() throws Exception {
-        dateParsesWithoutError("2010-07-21T03:18:58.000+00:00");
-        dateParsesWithoutError("2011-06-30T21:17:48.000+00:00");
-        dateParsesWithoutError("2015-11-17T06:30:02.000+00:00");
-        dateParsesWithoutError("2016-12-09T19:28:54.000+00:00");
-        dateParsesWithoutError("2015-02-05T05:20:02.000+00:00");
-        dateParsesWithoutError("2016-12-13T20:00:02.000+00:00");
+    public void dateParseSuccess() throws Exception {
+        String serverTimeStamp = "2010-07-21T03:18:58.000+00:00";
+
+        String expectedLocalizedTime = "Jul 20 08:18:58 PM -0700 2010";
+        String expectedLocalizedTimeFormat = "MMM dd KK:mm:ss aa ZZZ yyyy";
+        // Wed Jul 21 03:18:58 AM UTC 2010 // Time from server, always UTC standard
+        // Tue Jul 20 10:18:58 PM PST 2010 // Localized Los Angeles Time
+        // Both are the same time, just localized.
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(expectedLocalizedTimeFormat, Locale.getDefault());
+        simpleDateFormat.setTimeZone(TimeZone.getTimeZone("America/Los_Angeles"));
+        Date expectedDate = simpleDateFormat.parse(expectedLocalizedTime);
+        dateParseExpectedDateWithoutError(serverTimeStamp, expectedDate);
     }
 
-    private void dateParsesWithoutError(String sampleTime) throws Exception {
+    private void dateParseExpectedDateWithoutError(String sampleTime, Date expectedDate) throws Exception {
         Date date = DateUtil.dateFromString(sampleTime);
         assertNotNull(date);
+        assertEquals(expectedDate, date);
     }
 
 }
