0de734ad4a50229c4c088358552b9e60004a13c3
Jonathan Friesen <jonathanfriesen@gmail.com>
2018-03-27 11:44:13 -0700

    Remove app, bvlocation, bvstorenotifications and bvnotifications modules

--- bvnotifications/src/main/java/com/bazaarvoice/bvandroidsdk/ImageLoader.java
+++
@@ -1,9 +0,0 @@
-package com.bazaarvoice.bvandroidsdk;
-
-import android.graphics.Bitmap;
-import android.support.annotation.NonNull;
-import android.support.annotation.WorkerThread;
-
-interface ImageLoader {
-    @WorkerThread Bitmap loadImage(@NonNull String url);
-}
