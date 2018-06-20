22e9e5ee8308d8b604815cc748305ed63d6adb0b
Chris Pound <39925983+chris-pound-bazaarvoice@users.noreply.github.com>
2018-06-20 13:40:17 -0500

    MOB-851: Implement BVPixel in Recommendations
    * MOB-851: Implement BVPixel in Recommendations
    
    * MOB-851: Removes deprecated classes

---
+++ bvanalytics/src/main/java/com/bazaarvoice/bvandroidsdk/BVRecomendationImpressionEvent.java
@@ -0,0 +1,29 @@
+package com.bazaarvoice.bvandroidsdk;
+
+import java.util.Map;
+
+import static com.bazaarvoice.bvandroidsdk.BVAnalyticsUtils.mapPutSafe;
+
+public class BVRecomendationImpressionEvent extends BVMobileAnalyticsEvent {
+
+    private static final String bvProduct = "Recommendations";
+    private static final String KEY_VISIBLE = "visible";
+    private static final boolean VALUE_VISIBLE = true;
+    private static final String KEY_BV_PRODUCT = "bvProduct";
+    private static final String KEY_PRODUCT_ID = "productId";
+    private final String productId;
+
+    public BVRecomendationImpressionEvent(String productId) {
+        super(BVEventValues.BVEventClass.IMPRESSION, BVEventValues.BVEventType.RECOMMENDATIONS);
+        this.productId = productId;
+    }
+
+    @Override
+    public Map<String, Object> toRaw() {
+        Map<String, Object> map = super.toRaw();
+        mapPutSafe(map, KEY_VISIBLE, VALUE_VISIBLE);
+        mapPutSafe(map, KEY_PRODUCT_ID, productId);
+        mapPutSafe(map, KEY_BV_PRODUCT, bvProduct);
+        return map;
+    }
+}
