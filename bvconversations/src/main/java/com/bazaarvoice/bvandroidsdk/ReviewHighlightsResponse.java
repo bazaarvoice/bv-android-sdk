8d92d31e1e62aba6db7f802a6d9518700742692b
chetan shanbag <chetan.shanbag@bazaarvoice.com>
2020-07-24 13:11:08 +0530

    Implemented Error handling

--- bvconversations/src/main/java/com/bazaarvoice/bvandroidsdk/ReviewHighlightsResponse.java
+++ bvconversations/src/main/java/com/bazaarvoice/bvandroidsdk/ReviewHighlightsResponse.java
@@ -2,18 +2,31 @@ package com.bazaarvoice.bvandroidsdk;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 public class ReviewHighlightsResponse  extends ConversationsDisplayResponse {
 
    @SerializedName("subjects") private ReviewHighlights reviewHighlights;
+   @SerializedName("error") private String error;
+   private transient List<Error> errors;
+
 
     public  ReviewHighlights getReviewHighlights() {
         return reviewHighlights;
     }
 
     public Boolean getHasErrors() {
-        //TODO error handling changes
-        return false;
+        return error!= null ;
+    }
+
+    public List<Error> getErrors() {
+        if(errors == null){
+            Error reviewHighlightsError = new Error(error,null);
+            errors = new ArrayList<Error>(Arrays.asList(reviewHighlightsError));
+        }
+        return errors;
     }
 }
