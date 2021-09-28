d348ec6a65259b8015ec34b43de1824e11ee3059
BojanK <bojan.kseneman@gmail.com>
2021-09-28 12:19:00 +0200

    Make this library only depend on ads-identifier instead of the entire ads module

--- app/src/main/java/com/bazaarvoice/bvsdkdemoandroid/utils/DemoUtils.java
+++ app/src/main/java/com/bazaarvoice/bvsdkdemoandroid/utils/DemoUtils.java
@@ -7,8 +7,6 @@ import android.app.Activity;
 import android.graphics.Point;
 import android.view.Display;
 
-import com.google.android.gms.ads.AdRequest;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -16,22 +14,6 @@ public class DemoUtils {
 
     public static final int MAX_IMAGE_WIDTH = 1440;
     public static final int MAX_IMAGE_HEIGHT = 2560;
-
-//    private Picasso picasso;
-//    private Context context;
-//
-//    public DemoUtils(Context context) {
-//        this.context = context;
-//    }
-
-//    public Picasso picassoThumbnailLoader() {
-//        if (picasso == null) {
-//            picasso = new Picasso.Builder(context)
-//                    .defaultBitmapConfig(Bitmap.Config.RGB_565)
-//                    .build();
-//        }
-//        return picasso;
-//    }
 
     public static <ReturnType> ReturnType safeParse(String key, JSONObject jsonObject) {
         return safeParse(key, jsonObject, null);
@@ -57,21 +39,6 @@ public class DemoUtils {
         } catch (JSONException e) {
         }
         return value != null ? value : defaultValue;
-    }
-
-    public static String getDfpAdErrorMessage(int errorCode) {
-        switch (errorCode) {
-            case AdRequest.ERROR_CODE_INTERNAL_ERROR:
-                return "Something happened internally; for instance, an invalid response was received from the ad server.";
-            case AdRequest.ERROR_CODE_NETWORK_ERROR:
-                return "The ad request was unsuccessful due to network connectivity.";
-            case AdRequest.ERROR_CODE_INVALID_REQUEST:
-                return "The ad request was invalid; for instance, the ad unit ID was incorrect.";
-            case AdRequest.ERROR_CODE_NO_FILL:
-                return "The ad request was successful, but no ad was returned due to lack of ad inventory.";
-            default:
-                return "unknown error code";
-        }
     }
 
     public static Point getScreenDimensions(Activity activity) {
